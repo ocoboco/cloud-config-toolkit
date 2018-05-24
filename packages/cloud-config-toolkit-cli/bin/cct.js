9ee1112f0de90938c12cb9dc2fe882a406ef8d52
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-05-24 16:57:14 +0300

    Validate command

--- packages/cloud-config-toolkit-cli/bin/cct.js
+++ packages/cloud-config-toolkit-cli/bin/cct.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
+const yargs = require('yargs');
+const resolveCwd = require('resolve-cwd');
 let cctConfig;
 
 try {
   config = require(process.cwd() + '/cct-config.js');
 } catch (error) {
-  console.log(chalk.yellow('cct-config.js') + chalk.red(' must be specified in the project root directory.\n'));
+  console.log(chalk.red('Error: ') + chalk.yellow('cct-config.js') + ' not found in the project directory\n');
   throw error;
 }
+
+const argv = yargs
+  .commandDir('commands')
+  .demandCommand()
+  .help()
+  .argv;
---
+++ packages/cloud-config-toolkit-cli/bin/commands/validate.js
@@ -0,0 +1,6 @@
+exports.command = 'validate <configPath>'
+exports.desc = 'Validate the configuration file';
+
+exports.handler = function (argv) {
+  console.log('Validate called for configPath', argv.configPath);
+};
