587bd54c67030f26beb98ed6bb059a7438984fa8
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-06-04 12:42:31 +0300

    Exporter unit tests

--- packages/cloud-config-toolkit-ajv/lib/exporter.js
+++ packages/cloud-config-toolkit-ajv/lib/exporter.js
@@ -1,16 +1,18 @@
+const R = require('ramda');
+
 const AjvCreator = require('./ajv-creator');
 
 class Exporter {
   constructor({ schema, keywords = [] }) {
     this.schema = schema;
-    this.ajvInstance = AjvCreator.creator(keywords);
+    this.ajvInstance = AjvCreator.create(keywords);
   }
 
   export(configuration) {
     const exportedConfiguration = R.clone(configuration);
-    const valid = ajv.validate(this.schema, exportedConfiguration);
+    const valid = this.ajvInstance.validate(this.schema, exportedConfiguration);
     if (!valid) {
-      throw new Error('Configuration is not valid.'); 
+      throw new Error('Cannot export an invalid configuration.'); 
     }
     return exportedConfiguration;
   }
--- packages/cloud-config-toolkit-ajv/lib/tests/exporter.test.js
+++ packages/cloud-config-toolkit-ajv/lib/tests/exporter.test.js
@@ -1,9 +1,27 @@
 const Ajv = require('ajv');
 
-const Modification = require('../exporter');
+const Exporter = require('../exporter');
+const keywords = require('./fixtures/keywords');
+const schema = require('./fixtures/schema');
+const validConfig = require('./fixtures/config/valid.json');
+const invalidConfig = require('./fixtures/config/invalid.json');
 
 describe('Exporter', function() {
-  test('modifies configuration according to schema', function() {
-    expect(true).toBeTruthy();
+  test('exports valid configuration', function() {
+    const exporter = new Exporter({
+      schema,
+      keywords
+    });
+    expect(exporter.export(validConfig)).toMatchSnapshot();
+  });
+
+  test('throws an error when configuration is invalid', function() {
+    const exporter = new Exporter({
+      schema,
+      keywords
+    });
+    expect(function() {
+      expect(exporter.export(invalidConfig)).toMatchSnapshot();
+    }).toThrow('invalid');
   });
 });
