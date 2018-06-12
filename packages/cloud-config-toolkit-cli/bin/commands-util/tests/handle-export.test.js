897489befd534838dd018b1b076149f834fa4ebe
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-06-12 18:58:52 +0300

    Export command unit tests

--- packages/cloud-config-toolkit-cli/bin/commands-util/tests/handle-export.test.js
+++ packages/cloud-config-toolkit-cli/bin/commands-util/tests/handle-export.test.js
@@ -1,9 +1,11 @@
 const mock = require('mock-fs');
+const fs = require('fs-extra');
 
 const handleExport = require('../handle-export');
 
 const toolkit = {
   deserialize: JSON.parse,
+  serialize: JSON.stringify,
   validate() {
     return {
       isValid: true, 
@@ -18,23 +20,45 @@ const toolkit = {
   }
 };
 
-// describe('handleExport()', function() {
-//   test('throws an error when destination file already exists', async function() {
-//     try {
-//       await handleExport(toolkit, {
-//         path: 'invalid-path-config.json'
-//       });
-//     } catch (e) {
-//       expect(e.message).toBe("ENOENT, no such file or directory 'invalid-path-config.json'");
-//     }
-//   });
-// });
+describe('handleExport()', function() {
+  test('throws an error when destination file already exists', async function() {
+    try {
+      await handleExport(toolkit, {
+        path: 'config.json',
+        destination: 'exportedConfig.json'
+      });
+    } catch (e) {
+      expect(e.message).toMatchSnapshot();
+    }
+  });
+
+  test('throws an error when destination directory does not exist', async function() {
+    try {
+      await handleExport(toolkit, {
+        path: 'config.json',
+        destination: 'no-dir/exportedConfig.json'
+      });
+    } catch (e) {
+      expect(e.message).toMatchSnapshot();
+    }
+  });
+
+  test('writes exported config to destination file', async function() {
+    await handleExport(toolkit, {
+      path: 'config.json',
+      destination: 'config-dir/exportedConfig.json'
+    });
+    const fileContent = await fs.readFile('config-dir/exportedConfig.json', 'utf8');
+    expect(fileContent).toMatchSnapshot();
+  });
+});
 
 beforeAll(function() {
   console.log = jest.fn();
   mock({
-    'invalid-config.json': '{ "prop": "value" ',
     'config.json': '{ "prop": "value" }',
+    'exportedConfig.json': '{ "prop": "value", "exported": true }',
+    'config-dir/': {}
   });
 });
 
